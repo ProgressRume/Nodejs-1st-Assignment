50f0dc18dd4172cb82b2b92b6e604adfb33560f7
ProgressRume <awesomeluv94@gmail.com>
2023-03-26 22:13:59 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//Create a javascript file and inside the file, add a code that will log in to the console, your name, 
+// your preferred programming language, and where you see yourself in the next 1 year.
+
+let myName = "Progress Oghenerume";
+let preferredProgrammingLanguage = "JavaScript";
+let nextFiveYears = "Backend Engineer";
+console.log('My name is ' + myName + ', my preferred programming language is ' + preferredProgrammingLanguage + ', in the next 1 year, I want to be a ' + nextFiveYears)
